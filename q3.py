f11920316784cc27b922c3d02fad75c39fb13454
BarVinizky <155451112+BarVinizky@users.noreply.github.com>
2024-01-29 19:47:29 +0200

    Update q3.py

--- q3.py
+++ q3.py
@@ -12,14 +12,14 @@ if __name__ == '__main__':
     # years) and in March(February-March).
 
     cursor.execute("""
-        SELECT AVG(new_tests)
+        SELECT AVG(new_tests) # calculate the average amount of new tests
         FROM covid_vaccination
-        WHERE positive_rate > (
-                   SELECT AVG(positive_rate)
-                   FROM covid_vaccination
-                   WHERE positive_rate <> ""
-                   )
-        AND new_tests <> ""
+        WHERE new_tests <> '' AND positive_rate <> '' # not empty
+            # if positive rate  is higher than average
+            AND positive_rate > (SELECT AVG(positive_rate) # calculate the average of positive rate
+                                 FROM covid_vaccination
+                                 # not empty
+                                 WHERE new_tests <> '' AND positive_rate <> '')
      """)
     print(', '.join(str(row) for row in cursor.fetchall()))
 
