f254e56d240bb594075771c1a453d85c77ceb33a
BarVinizky <155451112+BarVinizky@users.noreply.github.com>
2024-01-29 19:48:21 +0200

    Update q4.py

--- q4.py
+++ q4.py
@@ -12,11 +12,12 @@ if __name__ == '__main__':
     # years) and in March(February-March).
 
     cursor.execute("""
-        SELECT date, Sum(new_vaccinations) as daily_vaccinations
+        # select dates and calculate the sum amount of new vaccinations
+        SELECT date, SUM(new_vaccinations) AS daily_vaccinations
         FROM covid_vaccination
-        WHERE continent = 'Asia'
-        GROUP BY date
-        HAVING Sum(new_vaccinations) > 20000000
+        WHERE continent = 'Asia' # in the continent Asia
+        GROUP BY date # groups the results by date
+        HAVING SUM(new_vaccinations) > 20000000 #  the sum amount of new vaccinations is more than 20000000
      """)
     print(', '.join(str(row) for row in cursor.fetchall()))
 
