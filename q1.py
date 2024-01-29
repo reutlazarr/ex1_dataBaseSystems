9397e5a2141ce2d69b573e7b197cd17e46bbb32a
Reut Lazar <reootlazar@gmail.com>
2024-01-29 13:01:53 +0200

    q1.py

--- q1.py
+++ q1.py
@@ -1,17 +1,26 @@
 import mysql.connector
 if __name__ == '__main__':
     mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="covid_db",
-        port='3307',
-        )
+    host="localhost",
+    user="root",
+    password="root",
+    database="covid_db",
+    port='3307',
+    )
     cursor = mydb.cursor()
     # Find the difference between the number of total cases in February (across all
     # years) and in March(February-March).
 
     cursor.execute("""
-    ## PUT YOUR QUERY ##
+     SELECT
+        (SELECT SUM(new_cases)
+        FROM covid_deaths
+        WHERE date LIKE '%/02/%') -
+        (SELECT SUM(new_cases)
+        FROM covid_deaths
+        WHERE date LIKE '%/03/%') AS difference;
     """)
     print(', '.join(str(row) for row in cursor.fetchall()))
+
+
+    
