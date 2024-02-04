0baa583da03c4e4fd4e2ff699d7e06d4ba733ff3
BarVinizky <155451112+BarVinizky@users.noreply.github.com>
2024-02-04 16:44:36 +0200

    Update q7.py

--- q7.py
+++ q7.py
@@ -1,32 +1,21 @@
-# Write a query such that For each location in covid_deaths, will find:
-# a. The monthly average of new cases(called “avg”)
-# b. The max monthly amount of new cases(called “max_monthly”)
-# c. The min monthly amount of new cases(called “min_monthly”)
-# The result should present the location, and all 3 amount
-
-# SELECT location, 
-# we need to take each month and do an averge
-# take all the days in month and make average. and make averge to this
-
-
 import mysql.connector
+
 if __name__ == '__main__':
     mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database="covid_db",
-    port='3307',
+        host="localhost",
+        user="root",
+        password="root",
+        database="covid_db",
+        port='3307',
     )
     cursor = mydb.cursor()
     cursor.execute("""
-    SELECT location, AVG(monthly_avg) AS avg_monthly_average
-    FROM (
-        SELECT location, MONTH(date) AS month, AVG(new_cases) AS monthly_avg
-        FROM covid_deaths
-        GROUP BY location, MONTH(date)
-    ) AS monthly_averages
-    GROUP BY location;
-     """) 
+    # select location,  monthly average, max monthly amount and min monthly amount of new cases
+        SELECT location, AVG(sum_new_cases) AS avg, MAX(sum_new_cases) AS max_monthly, MIN(sum_new_cases) AS min_monthly
+        FROM (SELECT location, SUM(new_cases) AS sum_new_cases # select the sum of new cases per location and month
+                FROM covid_deaths
+                GROUP BY location, MONTH(date)) AS monthly_stats_per_location # group by location and monthly
+        GROUP BY location # group by location
+    """)
     print(', '.join(str(row) for row in cursor.fetchall()))
 
