223dcd68f8a708a28f1353b074302298eae8938d
Reut Lazar <reootlazar@gmail.com>
2024-02-04 16:04:12 +0200

    q6.py

--- q6.py
+++ q6.py
@@ -9,13 +9,14 @@ if __name__ == '__main__':
     )
     cursor = mydb.cursor()
     cursor.execute("""
-        SELECT continent
-        FROM covid_deaths
-        GROUP BY continent
-        HAVING SUM(new_cases) > (SELECT AVG(new_cases) 
-                                    FROM covid_deaths
-                                    WHERE location NOT LIKE 'A%'
-                                )
+        SELECT continent # select continent
+        FROM  covid_deaths
+        GROUP BY continent # group by continent
+        # for each group the overall amount of new cases is greater than the average number over all the continents
+        HAVING SUM(new_cases) > (SELECT AVG(new_cases)
+                                    FROM  covid_deaths
+                                    # remove from the locations that start with ‘A’ character
+                                    WHERE location NOT LIKE 'A%')
      """)
     print(', '.join(str(row) for row in cursor.fetchall()))
 
